949d6a8e9b588968742155554713f0374ba1dd6c
d-komissarchik <0667669609@ukr.net>
2023-08-08 13:37:06 +0300

    edited

--- src/scraping/parsers.py
+++ src/scraping/parsers.py
@@ -3,18 +3,17 @@ import codecs
 from bs4 import BeautifulSoup as BS
 from random import randint
 
-
 __all__ = ('work', 'dou', 'djinni')
 
 headers = [
     {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
-    {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
+     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
+    {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
     {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-    ]
-
+     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+]
 
 
 def work(url):
@@ -87,11 +86,11 @@ def work(url):
 def dou(url):
     jobs = []
     errors = []
-    #domain = 'https://www.work.ua'
+    # domain = 'https://www.work.ua'
     resp = requests.get(url, headers=headers[randint(0, 2)])
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
-        main_div = soup.find('div', id= 'vacancyListId')
+        main_div = soup.find('div', id='vacancyListId')
         if main_div:
             li_list = main_div.find_all('li', attrs={'class': 'l-vacancy'})
             for li in li_list:
@@ -100,7 +99,7 @@ def dou(url):
                     href = title.a['href']
                     cont = li.find('div', attrs={'class': 'sh-info'})
                     content = cont.text
-                    company = '' #'No name'
+                    company = ''  # 'No name'
                     a = li.find('a', attrs={'class': 'company'})
                     if a:
                         company = a.text
@@ -111,6 +110,7 @@ def dou(url):
     else:
         errors.append({'url': url, 'title': "Page don't response"})
     return jobs, errors
+
 
 def djinni(url):
     jobs = []
@@ -147,6 +147,3 @@ if __name__ == '__main__':
     h = codecs.open('work.txt', 'w', 'utf-8')
     h.write(str(jobs))
     h.close()
-
-
-
